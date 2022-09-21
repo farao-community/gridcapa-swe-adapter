cf542184c6a6fa84d6f6f49b16195aa3c1b44f25
pascolithe <theo.pascoli@rte-france.com>
2022-09-21 14:23:32 +0200

    add tests

--- gridcapa-swe-adapter-app/src/test/java/com/farao_community/farao/swe/adapter/app/SweAdapterListenerTest.java
+++ gridcapa-swe-adapter-app/src/test/java/com/farao_community/farao/swe/adapter/app/SweAdapterListenerTest.java
@@ -19,7 +19,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.eq;
 
 /**
