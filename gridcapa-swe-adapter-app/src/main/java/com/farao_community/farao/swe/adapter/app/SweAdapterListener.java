9c2e4ca0fa1c1783800a2c4a7359fceb6674bd25
schwitzguebel <37381897+schwitzguebel@users.noreply.github.com>
2024-08-30 10:34:39 +0200

    Add interrupt pending feature (#83)
    * Add interrupt pending feature
    
    * Update dependency versions
    
    * Update after review
    
    * Increase Unit tests coverage
    
    * fix method name
    
    ---------
    
    Co-authored-by: SCHWITZGUEBEL Marc Ext <schwitzguebelmar@rte-france.com>

--- gridcapa-swe-adapter-app/src/main/java/com/farao_community/farao/swe/adapter/app/SweAdapterListener.java
+++ gridcapa-swe-adapter-app/src/main/java/com/farao_community/farao/swe/adapter/app/SweAdapterListener.java
@@ -1,11 +1,12 @@
 /*
- * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 package com.farao_community.farao.swe.adapter.app;
 
+import com.farao_community.farao.gridcapa.task_manager.api.ProcessRunDto;
 import com.farao_community.farao.gridcapa_swe_commons.exception.SweInvalidDataException;
 import com.farao_community.farao.gridcapa_swe_commons.resource.ProcessType;
 import com.farao_community.farao.gridcapa.task_manager.api.ProcessFileDto;
@@ -67,6 +68,7 @@ public class SweAdapterListener {
 
     SweRequest getManualSweRequest(TaskDto taskDto) {
         return new SweRequest(taskDto.getId().toString(),
+                getCurrentRunId(taskDto),
                 getProcessTypeFromConfiguration(),
                 taskDto.getTimestamp(),
                 getFileRessourceFromInputs(taskDto.getInputs(), "CORESO_SV"),
@@ -103,5 +105,15 @@ public class SweAdapterListener {
                 .findFirst()
                 .orElseThrow(() -> new SweAdapterException("No file found for type " + type));
         return new SweFileResource(input.getFilename(), minioAadpter.generatePreSignedUrlFromFullMinioPath(input.getFilePath(), 1));
+    }
+
+    private String getCurrentRunId(TaskDto taskDto) {
+        List<ProcessRunDto> runHistory = taskDto.getRunHistory();
+        if (runHistory == null || runHistory.isEmpty()) {
+            LOGGER.warn("Failed to handle manual run request on timestamp {} because it has no run history", taskDto.getTimestamp());
+            throw new SweAdapterException("Failed to handle manual run request on timestamp because it has no run history");
+        }
+        runHistory.sort((o1, o2) -> o2.getExecutionDate().compareTo(o1.getExecutionDate()));
+        return runHistory.get(0).getId().toString();
     }
 }
--- gridcapa-swe-adapter-app/src/test/java/com/farao_community/farao/swe/adapter/app/SweAdapterListenerTest.java
+++ gridcapa-swe-adapter-app/src/test/java/com/farao_community/farao/swe/adapter/app/SweAdapterListenerTest.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.farao_community.farao.swe.adapter.app;
 
 import com.farao_community.farao.gridcapa.task_manager.api.*;
@@ -15,6 +21,7 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 /**
@@ -32,31 +39,43 @@ class SweAdapterListenerTest {
     @Test
     void testGetManualSweRequest() {
         when(minioAdapter.generatePreSignedUrlFromFullMinioPath("filePath", 1)).thenReturn("filePathUrl");
-        TaskDto taskDto = createTaskDtoWithStatus(TaskStatus.READY);
+        TaskDto taskDto = createTaskDto(true);
         SweRequest sweRequest = sweAdapterListener.getManualSweRequest(taskDto);
         assertEquals(taskDto.getId().toString(), sweRequest.getId());
         assertEquals(taskDto.getTimestamp(), sweRequest.getTargetProcessDateTime());
     }
 
-    TaskDto createTaskDtoWithStatus(TaskStatus status) {
+    @Test
+    void testGetManualSweRequestNoRunHistory() {
+        when(minioAdapter.generatePreSignedUrlFromFullMinioPath("filePath", 1)).thenReturn("filePathUrl");
+        TaskDto taskDto = createTaskDto(false);
+        assertThrows(SweAdapterException.class, () -> sweAdapterListener.getManualSweRequest(taskDto));
+    }
+
+    TaskDto createTaskDto(boolean withRunHistory) {
         UUID id = UUID.randomUUID();
         OffsetDateTime timestamp = OffsetDateTime.parse("2022-09-20T10:30Z");
         List<ProcessFileDto> processFiles = new ArrayList<>();
-        processFiles.add(new ProcessFileDto("filePath", "CORESO_SV", ProcessFileStatus.VALIDATED, "fileName", OffsetDateTime.now()));
-        processFiles.add(new ProcessFileDto("filePath", "REE_EQ", ProcessFileStatus.VALIDATED, "fileName", OffsetDateTime.now()));
-        processFiles.add(new ProcessFileDto("filePath", "REE_TP", ProcessFileStatus.VALIDATED, "fileName", OffsetDateTime.now()));
-        processFiles.add(new ProcessFileDto("filePath", "REE_SSH", ProcessFileStatus.VALIDATED, "fileName", OffsetDateTime.now()));
-        processFiles.add(new ProcessFileDto("filePath", "REN_EQ", ProcessFileStatus.VALIDATED, "fileName", OffsetDateTime.now()));
-        processFiles.add(new ProcessFileDto("filePath", "REN_TP", ProcessFileStatus.VALIDATED, "fileName", OffsetDateTime.now()));
-        processFiles.add(new ProcessFileDto("filePath", "REN_SSH", ProcessFileStatus.VALIDATED, "fileName", OffsetDateTime.now()));
-        processFiles.add(new ProcessFileDto("filePath", "RTE_EQ", ProcessFileStatus.VALIDATED, "fileName", OffsetDateTime.now()));
-        processFiles.add(new ProcessFileDto("filePath", "RTE_TP", ProcessFileStatus.VALIDATED, "fileName", OffsetDateTime.now()));
-        processFiles.add(new ProcessFileDto("filePath", "RTE_SSH", ProcessFileStatus.VALIDATED, "fileName", OffsetDateTime.now()));
-        processFiles.add(new ProcessFileDto("filePath", "CRAC", ProcessFileStatus.VALIDATED, "fileName", OffsetDateTime.now()));
-        processFiles.add(new ProcessFileDto("filePath", "GLSK", ProcessFileStatus.VALIDATED, "fileName", OffsetDateTime.now()));
-        processFiles.add(new ProcessFileDto("filePath", "BOUNDARY_EQ", ProcessFileStatus.VALIDATED, "fileName", OffsetDateTime.now()));
-        processFiles.add(new ProcessFileDto("filePath", "BOUNDARY_TP", ProcessFileStatus.VALIDATED, "fileName", OffsetDateTime.now()));
-        return new TaskDto(id, timestamp, status, processFiles, null, null, null, Collections.emptyList(), Collections.emptyList());
+        processFiles.add(new ProcessFileDto("filePath", "CORESO_SV", ProcessFileStatus.VALIDATED, "fileName", "documentId", OffsetDateTime.now()));
+        processFiles.add(new ProcessFileDto("filePath", "REE_EQ", ProcessFileStatus.VALIDATED, "fileName", "documentId", OffsetDateTime.now()));
+        processFiles.add(new ProcessFileDto("filePath", "REE_TP", ProcessFileStatus.VALIDATED, "fileName", "documentId", OffsetDateTime.now()));
+        processFiles.add(new ProcessFileDto("filePath", "REE_SSH", ProcessFileStatus.VALIDATED, "fileName", "documentId", OffsetDateTime.now()));
+        processFiles.add(new ProcessFileDto("filePath", "REN_EQ", ProcessFileStatus.VALIDATED, "fileName", "documentId", OffsetDateTime.now()));
+        processFiles.add(new ProcessFileDto("filePath", "REN_TP", ProcessFileStatus.VALIDATED, "fileName", "documentId", OffsetDateTime.now()));
+        processFiles.add(new ProcessFileDto("filePath", "REN_SSH", ProcessFileStatus.VALIDATED, "fileName", "documentId", OffsetDateTime.now()));
+        processFiles.add(new ProcessFileDto("filePath", "RTE_EQ", ProcessFileStatus.VALIDATED, "fileName", "documentId", OffsetDateTime.now()));
+        processFiles.add(new ProcessFileDto("filePath", "RTE_TP", ProcessFileStatus.VALIDATED, "fileName", "documentId", OffsetDateTime.now()));
+        processFiles.add(new ProcessFileDto("filePath", "RTE_SSH", ProcessFileStatus.VALIDATED, "fileName", "documentId", OffsetDateTime.now()));
+        processFiles.add(new ProcessFileDto("filePath", "CRAC", ProcessFileStatus.VALIDATED, "fileName", "documentId", OffsetDateTime.now()));
+        processFiles.add(new ProcessFileDto("filePath", "GLSK", ProcessFileStatus.VALIDATED, "fileName", "documentId", OffsetDateTime.now()));
+        processFiles.add(new ProcessFileDto("filePath", "BOUNDARY_EQ", ProcessFileStatus.VALIDATED, "fileName", "documentId", OffsetDateTime.now()));
+        processFiles.add(new ProcessFileDto("filePath", "BOUNDARY_TP", ProcessFileStatus.VALIDATED, "fileName", "documentId", OffsetDateTime.now()));
+        ArrayList<ProcessRunDto> runHistory = null;
+        if (withRunHistory) {
+            runHistory = new ArrayList<>();
+            runHistory.add(new ProcessRunDto(UUID.randomUUID(), OffsetDateTime.now(), Collections.emptyList()));
+        }
+        return new TaskDto(id, timestamp, TaskStatus.READY, processFiles, null, null, null, runHistory, Collections.emptyList());
     }
 
 }
